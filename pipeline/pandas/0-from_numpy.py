495abac4204647da6ba6ba14a7d25074b30a428b
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-01-05 11:43:33 +0000

    task

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -8,7 +8,7 @@ import pandas as pd
 def from_numpy(array):
     """ I hate docstring... """
 
-    result = pd.DataFrame(array, columns=map(lambda x:chr(x), range(65, 91)))
+    result = pd.DataFrame(array, columns=list(map(lambda x:chr(x), range(65, 65+array.shape[1]))))
 
 
     return result
