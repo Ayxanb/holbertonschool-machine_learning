c12420d9c42f96734ef8c6a7bcacfc4e88e5d812
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-01-05 13:27:51 +0000

    task

---
+++ pipeline/pandas/4-array.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+''' this is a docstring :-) '''
+
+
+import pandas as pd
+
+
+def array(df):
+    '''
+    takes a pd.DataFrame as input and performs the following:
+
+    df is a pd.DataFrame containing columns named High and Close.
+    The function should select the last 10 rows of the High and Close columns.
+    Convert these selected values into a numpy.ndarray.
+    Returns: the numpy.ndarray
+    '''
+
+    df = df.tail(10)
+    return df.to_numpy()
