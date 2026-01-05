abedb45b7d87419ce6919de5ec0ca527d2c45b83
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-01-05 12:13:23 +0000

    task

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -7,8 +7,9 @@ import pandas as pd
 
 df = pd.DataFrame(
     {
-        "First": pd.Series([0.0, 0.5, 1.0, 1.5])
-        "Second": pd.Series(['one', 'two', 'three', 'four'])
+        "First": pd.Series([1], index=list('AB')),
+        "Second": pd.Series([2])
     },
-    columns=list('ABCD')
 )
+
+print(df)
