b5dd85c2c3292c45facabe36b821550cc333b3c6
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-01-05 12:02:19 +0000

    task

---
+++ pipeline/pandas/1-from_dictionary.py
@@ -0,0 +1,13 @@
+''' This module creates a pd.DataFrame object from a Python dictionary  '''
+
+
+import pandas as pd
+
+
+df = pd.DataFrame(
+    {
+        "First": pd.Series([0.0, 0.5, 1.0, 1.5])
+        "Second": pd.Series(['one', 'two', 'three', 'four'])
+    },
+    columns=list('ABCD')
+)
