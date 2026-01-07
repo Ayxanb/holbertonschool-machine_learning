10f0f5435d477fc888087158d08bd51b683f76d9
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-01-07 09:48:22 +0000

    task

---
+++ pipeline/pandas/6-flip_switch.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+'''
+This module contains `flip_switch` function
+'''
+
+def flip_switch(df):
+    '''
+    Sorts the DataFrame in reverse chronological order by its index,
+    then transposes it.
+    '''
+    return df.sort_index(ascending=False).T
