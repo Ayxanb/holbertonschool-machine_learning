e6927bfd66628a541f651debfd4df3931daa93c4
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-01-07 11:38:06 +0000

    task

--- pipeline/pandas/13-analyze.py
+++ pipeline/pandas/13-analyze.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
-def analyze(df):
+'''
+This module provides `analyze` function
+'''
 
+
+def analyze(df):
+    '''
+    takes a pd.DataFrame and:
+
+    Computes descriptive statistics for all columns except the Timestamp column.
+    Returns a new pd.DataFrame containing these statistics.
+    '''
+
+    return df.drop(columns=["Timestamp"]).describe()
