d7da773cb71a2227ac643b492d6a36b8999fcef6
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-01-11 08:51:54 +0000

    task

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python3
-
-'''
-'''
+"""
+This module provides a function to create a scatter plot showing
+the relationship between men's height and weight.
+"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def scatter():
-    '''
-    '''
+    """
+    Generates and displays a scatter plot of men's height versus weight.
+
+    The data is randomly generated using a multivariate normal distribution.
+    Heights are measured in inches, weights in pounds.
+    """
 
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
+
     np.random.seed(5)
     x, y = np.random.multivariate_normal(mean, cov, 2000).T
     y += 180
+
     plt.figure(figsize=(6.4, 4.8))
-
-    plt.title('Men\'s Height vs Weight')
-    plt.xlabel('Height (in)')
-    plt.ylabel('Weight (lbs)')
-
+    plt.title("Men's Height vs Weight")
+    plt.xlabel("Height (in)")
+    plt.ylabel("Weight (lbs)")
     plt.scatter(x, y, color='magenta')
     plt.show()
