da133936b3a53524f325123f6aa56d699e950fcc
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-01-11 08:37:54 +0000

    task

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,20 +1,11 @@
 #!/usr/bin/env python3
-
-'''
-This module provides a `line` function which plots x^3
-'''
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def line():
-    '''
-    plots x^3
-    '''
+
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
     plt.plot(y, color='red')
-
     plt.show()
