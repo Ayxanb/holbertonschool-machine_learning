5fc53b4350b9716d39368896c2568360b55c8845
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-01-11 09:50:35 +0000

    task

--- math/plotting/2-change_scale.py
+++ math/plotting/2-change_scale.py
@@ -10,12 +10,8 @@ import matplotlib.pyplot as plt
 
 def change_scale():
     """
-    Plots the fraction of Carbon-14 remaining over time.
-
-    The x-axis represents time in years, and the y-axis represents
-    the fraction remaining on a logarithmic scale.
+    Plots the exponential decay of Carbon-14 on a semi-logarithmic scale.
     """
-
     x = np.arange(0, 28651, 5730)
     r = np.log(0.5)
     t = 5730
@@ -23,10 +19,9 @@ def change_scale():
 
     plt.figure(figsize=(6.4, 4.8))
     plt.plot(x, y)
+    plt.yscale('log')
     plt.xlabel('Time (years)')
     plt.ylabel('Fraction Remaining')
     plt.title('Exponential Decay of C-14')
     plt.xlim(0, 28650)
-    plt.yscale('log')
-    plt.ylim(y[-1], 1)
     plt.show()
