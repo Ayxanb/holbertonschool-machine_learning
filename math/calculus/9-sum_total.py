fbb64c94f773190f6273d36afb1f2019701a4d0e
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-01-12 13:15:46 +0000

    task

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -9,4 +9,7 @@ def summation_i_squared(n):
     Calculates the sum of squares from 1 to n using the closed-form formula.
     Formula used: n(n + 1)(2n + 1) / 6
     """
+    if not isinstance(n, int) or n < 0:
+        return None
+
     return n * (n + 1) * (2 * n + 1) / 6
