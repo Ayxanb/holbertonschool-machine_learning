97710f36168fbb497ae240d43da41af95fca9a0f
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-01-21 10:18:52 +0000

    task

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -7,7 +7,7 @@ This module contains `add_matrices2D` function
 
 def add_matrices2D(mat1, mat2):
     '''
-    adds two 2D matrices 
+    adds two 2D matrices
     '''
 
     matrix_shape = __import__('2-size_me_please').matrix_shape
@@ -15,8 +15,5 @@ def add_matrices2D(mat1, mat2):
     if matrix_shape(mat1) != matrix_shape(mat2):
         return None
 
-    return [[a + b for a, b in zip(arr1, arr2)] for arr1, arr2 in zip(mat1, mat2)]
-
-mat1 = [[1, 2], [3, 4]]
-mat2 = [[5, 6], [7, 8]]
-print(add_matrices2D(mat1, mat2))
+    return [[a + b for a, b in zip(arr1, arr2)]
+            for arr1, arr2 in zip(mat1, mat2)]
