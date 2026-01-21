6257202f338e57e5e717b8c16843d005a7fb3aff
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-01-21 11:15:38 +0000

    task

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+'''
+This module contains `car_arrays` function
+'''
+
+
+def cat_arrays(arr1, arr2):
+    '''
+    concatinates two arrays
+    '''
+    result = []
+    result.extend(arr1)
+    result.extend(arr2)
+    return result
