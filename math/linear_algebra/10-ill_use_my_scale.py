2b05b472c7b464af91244728d8f797b0bac43a4f
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-01-31 13:50:17 +0000

    task

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,12 @@
+'''
+This module contains `np_shape` function
+'''
+
+import numpy as np
+
+
+def np_shape(matrix):
+    """
+    Returns the shape of a numpy.ndarray as a tuple.
+    """
+    return matrix.shape
