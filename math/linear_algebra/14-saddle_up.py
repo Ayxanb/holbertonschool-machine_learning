e7b716da8bdfb682645d256e4541adc9cd65ec95
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-01-31 14:00:26 +0000

    task

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+This module contains `np_matmul` function
+'''
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """
+    Performs matrix multiplication on two numpy.ndarrays.
+    """
+    return np.matmul(mat1, mat2)
