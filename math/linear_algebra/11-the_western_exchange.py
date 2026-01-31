34fd116a631320033898b22e93a8db59c93b4909
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-01-31 13:54:11 +0000

    task

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+This module contains `np_transpose` function
+'''
+
+
+def np_transpose(matrix):
+    '''
+    Return the transpose matrix
+    '''
+
+    return matrix.T
