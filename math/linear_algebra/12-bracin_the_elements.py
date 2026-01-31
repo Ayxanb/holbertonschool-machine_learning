c0d6a40040de023de78efbd54d54ea93d02ccf08
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-01-31 13:57:27 +0000

    task

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -8,5 +8,5 @@ def np_elementwise(mat1, mat2):
     """
     Returns a tuple of element-wise sum, difference, product, and quotient.
     """
-    
+
     return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
