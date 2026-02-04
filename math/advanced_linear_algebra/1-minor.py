92263b83fb8eb6c8a23b79c22b8838b3742a6b0e
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-02-04 12:17:06 +0000

    task

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -23,7 +23,7 @@ def minor(matrix):
         raise ValueError("matrix must be a non-empty square matrix")
 
     if rows == 1:
-        return matrix[0][0]
+        return [[1]]
 
     elif rows == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
