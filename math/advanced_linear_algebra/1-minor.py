c3cd03a12baf681cbfce68072bdc1db7423a9c0e
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-02-04 12:32:39 +0000

    task

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 '''
 This module contains `minor` function
 '''
@@ -7,35 +6,32 @@ This module contains `minor` function
 
 def minor(matrix):
     """
-    Calculates the determinant of a square matrix.
+    Calculates the minor matrix of a square matrix.
     """
 
     if not isinstance(matrix, list) or \
        not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
 
-    if matrix == [[]] or matrix == []:
+
+    if matrix == [] or matrix == [[]]:
         raise ValueError('matrix must be a non-empty square matrix')
 
     rows = len(matrix)
 
     if not all(len(row) == rows for row in matrix):
-        raise ValueError("matrix must be a non-empty square matrix")
-
+        raise ValueError("matrix must be a non-empty square matrix"y 
     if rows == 1:
         return [[1]]
 
-    elif rows == 2:
-        return [[matrix[1][1], matrix[1][0]], [matrix[0][1], matrix[0][0]]]
-
     determinant = __import__('0-determinant').determinant
-
     minor_matrix = []
-    for j in range(rows):
+
+    for i in range(rows):
         current_row = []
         for j in range(rows):
-            minor = [row[:j] + row[j+1:] for row in matrix[1:]]
-            current_row.append(determinant(minor))
+            sub_matrix = [row[:j] + row[j+1:] for row in (matrix[:i] + matrix[i+1:])]
+            current_row.append(determinant(sub_matrix))
         minor_matrix.append(current_row)
 
     return minor_matrix
