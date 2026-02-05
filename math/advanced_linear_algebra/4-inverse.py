dbc1a3d2f107146a74862b2c73d925268274f278
Ayxanb <ayxanbeydiyeb476@gmail.com>
2026-02-05 11:59:20 +0000

    task

--- math/advanced_linear_algebra/4-inverse.py
+++ math/advanced_linear_algebra/4-inverse.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
-
 '''
 This module contains `inverse` function
 '''
 
-
 def inverse(matrix):
     '''
-    returns the inverse of matrix
+    Calculates the inverse of a matrix
     '''
-
     determinant = __import__('0-determinant').determinant
     adjugate = __import__('3-adjugate').adjugate
-    
+
     if not isinstance(matrix, list) or \
        not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
@@ -21,18 +18,17 @@ def inverse(matrix):
         raise ValueError("matrix must be a non-empty square matrix")
 
     size = len(matrix)
-
     if not all(len(row) == size for row in matrix):
         raise ValueError("matrix must be a non-empty square matrix")
 
-    if size == 1:
-        return [[1]]
-
-
-    if determinant(matrix) == 0:
+    det = determinant(matrix)
+    if det == 0:
         return None
 
-    adjugate_matrix = adjugate(matrix)
-    det = determinant(matrix)
+    if size == 1:
+        return [[1 / matrix[0][0]]]
 
-    return list(map(lambda x: x/det, (row for row in adjugate_matrix)))
+    adj_mat = adjugate(matrix)
+    inv_matrix = [[val / det for val in row] for row in adj_mat]
+
+    return inv_matrix
