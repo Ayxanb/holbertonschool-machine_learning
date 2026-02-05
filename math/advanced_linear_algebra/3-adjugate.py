e90d444b3a8b0c6a83e13731784573184d1e228f
Ayxanb <ayxanbeydiyeb476@gmail.com>
2026-02-05 11:33:04 +0000

    task

--- math/advanced_linear_algebra/3-adjugate.py
+++ math/advanced_linear_algebra/3-adjugate.py
@@ -11,5 +11,20 @@ def adjugate(matrix):
     '''
     cofactor = __import__('2-cofactor').cofactor
 
+    if not isinstance(matrix, list) or \
+       not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+
+    if not matrix or not matrix[0]:
+        raise ValueError("matrix must be a non-empty square matrix")
+
+    size = len(matrix)
+
+    if not all(len(row) == size for row in matrix):
+        raise ValueError("matrix must be a non-empty square matrix")
+
+    if size == 1:
+        return [[1]]
+
     cofactor_matrix = cofactor(matrix)
     return [list(i) for i in zip(*matrix)]
