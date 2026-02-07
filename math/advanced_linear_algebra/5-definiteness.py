2064df67bcd5bc32e326e74f4cc77ec052ded4e1
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-02-07 18:13:16 +0000

    task

---
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+'''
+This module contains `definiteness` function
+'''
+
+import numpy as np
+
+
+def definiteness(matrix):
+    """
+    Determines the definiteness of a square matrix.
+    """
+
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("matrix must be a numpy.ndarray")
+    
+    if len(matrix.shape) != 2 or matrix.shape[0] != matrix.shape[1]:
+        return None
+    
+    try:
+        eigenvalues = np.linalg.eigvalsh(matrix)
+    except np.linalg.LinAlgError:
+        return None
+    
+    if np.all(eigenvalues > 0):
+        return "Positive definite"
+
+    elif np.all(eigenvalues >= 0):
+        return "Positive semi-definite"
+
+    elif np.all(eigenvalues < 0):
+        return "Negative definite"
+
+    elif np.all(eigenvalues <= 0):
+        return "Negative semi-definite"
+
+    elif np.any(eigenvalues > 0) and np.any(eigenvalues < 0):
+        return "Indefinite"
+
+    else:
+        return None
