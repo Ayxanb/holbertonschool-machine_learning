57acef1ea3e8e365d0ddc3f9162fe5f740ba61bf
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-02-10 14:10:11 +0000

    task

---
+++ math/probability/normal.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+'''
+This module contains `Normal` class
+'''
+
+
+class Normal:
+    '''
+    Normal distribution class
+    '''
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        '''
+        Initializes the Normal class
+        data: List of the data to be used to estimate the distribution
+        mean: The mean of the distribution
+        stddev: The standard deviation of the distribution
+        '''
+
+        if data is None:
+            if stddev <= 0:
+                raise ValueError('stddev must be a positive value')
+
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+
+        elif not isinstance(data, list):
+            raise TypeError('data must be a list')
+
+        elif len(data) < 2:
+            raise ValueError('data must contain multiple values')
+
+        else:
+            n = len(data)
+            self.mean = sum(data) / n
+            
+            _sum = 0
+            for i in range(1, n):
+                _sum += (data[i] - self.mean) ** 2
+
+            self.stddev = (_sum / n) ** 0.5
