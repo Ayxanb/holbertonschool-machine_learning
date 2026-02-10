40de6e7eed8c6d96c20be2d0836bd1a5690dc146
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-02-10 15:36:51 +0000

    task

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -39,3 +39,32 @@ class Binomial:
             p_initial = 1 - (variance / mean)
             self.n = int(round(mean / p_initial))
             self.p = float(mean / self.n)
+
+    def pmf(self, k):
+        """ Calculates the PMF for exactly k successes """
+        k = int(k)
+        if k < 0 or k > self.n:
+            return 0
+
+        def fact(num):
+            f = 1
+            for i in range(1, num + 1):
+                f *= i
+            return f
+
+        n_choose_k = fact(self.n) / (fact(k) * fact(self.n - k))        
+        return n_choose_k * (self.p ** k) * ((1 - self.p) ** (self.n - k))
+
+    def cdf(self, k):
+        """ Calculates the CDF for at most k successes """
+        k = int(k)
+        if k < 0:
+            return 0
+        if k >= self.n:
+            return 1
+
+        total_p = 0
+        for i in range(k + 1):
+            total_p += self.pmf(i)
+            
+        return total_p
