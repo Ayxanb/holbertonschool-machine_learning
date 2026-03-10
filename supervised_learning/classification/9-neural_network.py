44184efe8330dc9afb1db8d05f6c198c712309ef
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-03-10 14:01:42 +0000

    task

--- supervised_learning/classification/9-neural_network.py
+++ supervised_learning/classification/9-neural_network.py
@@ -1,29 +1,21 @@
-#!/usr/bin/env python3
 import numpy as np
 
 """
-This module constains `NeuralNetwork` class definition.
+This module contains definition of `NeuralNetwork` class.
 """
 
 
 class NeuralNetwork:
     """
-    Defines a neural network with one hidden layer performing binary
-    classification. This class encapsulates the weights and biases
-    required to transform input features into a binary prediction.
+    Defines a neural network with one hidden layer
+    performing binary classification with private attributes
     """
 
     def __init__(self, nx, nodes):
         """
-        Initializes the neural network.
-
-        Args:
-            nx (int): The number of input features.
-            nodes (int): The number of neurons in the hidden layer.
-
-        Raises:
-            TypeError: If nx or nodes are not integers.
-            ValueError: If nx or nodes are less than 1.
+        Initializes the neural network
+        nx: number of input features
+        nodes: number of nodes in the hidden layer
         """
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
@@ -34,47 +26,39 @@ class NeuralNetwork:
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
 
-        # Allows each hidden node to process every input feature.
         self.__W1 = np.random.normal(size=(nodes, nx))
-
-        # Column vector added to the hidden layer's linear product.
         self.__b1 = np.zeros((nodes, 1))
-
-        # __A1: Activated output for hidden layer.
         self.__A1 = 0
-
-        # Allows the output node to process every hidden layer output.
         self.__W2 = np.random.normal(size=(1, nodes))
-
         self.__b2 = 0
         self.__A2 = 0
 
     @property
     def W1(self):
-        """Weights vector for the hidden layer."""
+        """Getter for weights of the hidden layer"""
         return self.__W1
 
     @property
     def b1(self):
-        """Bias for the hidden layer."""
+        """Getter for bias of the hidden layer"""
         return self.__b1
 
     @property
     def A1(self):
-        """Activated output for the hidden layer."""
+        """Getter for activation of the hidden layer"""
         return self.__A1
 
     @property
     def W2(self):
-        """Weights vector for the output neuron."""
+        """Getter for weights of the output neuron"""
         return self.__W2
 
     @property
     def b2(self):
-        """Bias for the output neuron."""
+        """Getter for bias of the output neuron"""
         return self.__b2
 
     @property
     def A2(self):
-        """Activated output for the output neuron (prediction)."""
+        """Getter for activation of the output neuron"""
         return self.__A2
