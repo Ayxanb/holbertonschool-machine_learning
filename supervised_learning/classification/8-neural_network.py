dc0d7726f079dc0e52eb7b17d63e4ce4526ee7cc
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-03-10 14:10:30 +0000

    task

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
+"""
+This module defines a neural network with one hidden layer.
+The network is designed for binary classification tasks using
+a hidden layer of a specified size.
+"""
 import numpy as np
-
-"""
-This module contains the `NeuralNetwork` class,
-comninations of Neurons.
-"""
 
 
 class NeuralNetwork:
     """
-    Defines a neural network with one hidden layer
-    performing binary classification
+    Defines a neural network with one hidden layer performing
+    binary classification.
     """
 
     def __init__(self, nx, nodes):
@@ -18,28 +18,67 @@ class NeuralNetwork:
         Initializes the NeuralNetwork class.
 
         Args:
-            nx: number of input features
-            nodes: number of nodes in the hidden layer
-        """
+            nx (int): The number of input features.
+            nodes (int): The number of nodes in the hidden layer.
 
+        Raises:
+            TypeError: If nx or nodes are not integers.
+            ValueError: If nx or nodes are less than 1.
+        """
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
-
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-
         if not isinstance(nodes, int):
             raise TypeError("nodes must be an integer")
-
         if nodes < 1:
             raise ValueError("nodes must be a positive integer")
 
-        # Hidden layer: W1 shape is (nodes, nx), b1 shape is (nodes, 1)
-        self.W1 = np.random.normal(size=(nodes, nx))
-        self.b1 = np.zeros((nodes, 1))
-        self.A1 = 0
+        self.__W1 = np.random.normal(size=(nodes, nx))
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+        self.__W2 = np.random.normal(size=(1, nodes))
+        self.__b2 = 0
+        self.__A2 = 0
 
-        # Output neuron: W2 shape is (1, nodes), b2 is a scalar (or 1,1)
-        self.W2 = np.random.normal(size=(1, nodes))
-        self.b2 = 0
-        self.A2 = 0
+    @property
+    def W1(self):
+        """
+        The weights vector for the hidden layer.
+        """
+        return self.__W1
+
+    @property
+    def b1(self):
+        """
+        The bias for the hidden layer.
+        """
+        return self.__b1
+
+    @property
+    def A1(self):
+        """
+        The activated output for the hidden layer.
+        """
+        return self.__A1
+
+    @property
+    def W2(self):
+        """
+        The weights vector for the output neuron.
+        """
+        return self.__W2
+
+    @property
+    def b2(self):
+        """
+        The bias for the output neuron.
+        """
+        return self.__b2
+
+    @property
+    def A2(self):
+        """
+        The activated output for the output neuron (prediction).
+        """
+        return self.__A2
