2bbdc087bc1fd5511e771034d398d761e5aa033a
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-03-15 08:55:15 +0000

    task

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""
+This module contains a function to train a Keras model
+using mini-batch gradient descent.
+"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
+    """
+    Trains a model using mini-batch gradient descent.
+
+    Args:
+        network: the model to train
+        data: numpy.ndarray of shape (m, nx) containing the input data
+        labels: one-hot numpy.ndarray of shape (m, classes) containing the labels
+        batch_size: size of the batch used for mini-batch gradient descent
+        epochs: number of passes through data
+        verbose: boolean determining if output should be printed
+        shuffle: boolean determining whether to shuffle batches every epoch
+
+    Returns:
+        The History object generated after training the model.
+    """
+
+    history = network.fit(
+        x=data,
+        y=labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        verbose=verbose,
+        shuffle=shuffle
+    )
+
+    return history
