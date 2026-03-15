9418301d1c3060068285378e61d6d2a38a1d0495
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-03-15 08:56:22 +0000

    task

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -7,7 +7,7 @@ using mini-batch gradient descent.
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
+def train_model(network, data, labels, batch_size, epochs, verbose, shuffle):
     """
     Trains a model using mini-batch gradient descent.
 
