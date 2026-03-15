7148932bddb05bed0779fa01dc6d471faa5b3060
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-03-15 10:23:42 +0000

    task

---
+++ supervised_learning/keras/13-predict.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+This module contains a function to make predictions
+using a trained Keras model.
+"""
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    """
+    Makes a prediction using a neural network.
+
+    Args:
+        network: the network model to make the prediction with
+        data: the input data to make the prediction with
+        verbose: boolean determining if output should be printed 
+                 during the prediction process
+
+    Returns:
+        The prediction for the data (numpy.ndarray).
+    """
+    # The predict method returns the output of the last layer
+    prediction = network.predict(
+        x=data,
+        verbose=verbose
+    )
+
+    return prediction
