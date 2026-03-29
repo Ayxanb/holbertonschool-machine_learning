fff550e41b11d2c5454f3fb0c183e11ee184217b
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-03-29 13:56:16 +0000

    task

---
+++ math/convolutions_and_pooling/0-convole_grayscale_valid.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import numpy as np
+
+"""
+This module contains `convolve_grayscale_valid` function.
+"""
+
+
+def convolve_grayscale_valid(images, kernel):
+    """
+    Performs valid convolution on a batch of grayscale images.
+    """
+
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+
+    out_h = h - kh + 1
+    out_w = w - kw + 1
+
+    output = np.zeros((m, out_h, out_w))
+
+    for ky in range(kh):
+        for kx in range(kw):
+            output += images[:, ky:ky + out_h, kx:kx + out_w] * kernel[ky, kx]
+    return output
