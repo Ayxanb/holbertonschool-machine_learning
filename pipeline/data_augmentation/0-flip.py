191434e4a4a53c6392f1b2e9ca8a1e6776420371
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-04-12 15:27:33 +0000

    task

--- pipeline/data_augmentation/0-flip.py
+++ pipeline/data_augmentation/0-flip.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """
-This module provides utility functions for image preprocessing using TensorFlow.
-
-It includes operations for geometric transformations, specifically focusing 
-on horizontal flipping of image tensors to aid in data augmentation or 
+This module provides utility functions for image preprocessing.
+It includes operations for geometric transformations, specifically focusing
+on horizontal flipping of image tensors to aid in data augmentation or
 image manipulation tasks.
 """
 
@@ -15,7 +14,7 @@ def flip_image(image):
     Flips a 3D image tensor horizontally (left to right).
 
     Args:
-        image (tf.Tensor): A 3D tensor of shape [height, width, channels] 
+        image (tf.Tensor): A 3D tensor of shape [height, width, channels]
             representing the image.
 
     Returns:
