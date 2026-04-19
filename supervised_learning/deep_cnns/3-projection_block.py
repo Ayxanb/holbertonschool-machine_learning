893d8f4624d3333c402faffc63ba45b7ec855110
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-04-19 18:21:52 +0000

    task

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -9,7 +9,7 @@ from tensorflow import keras as K
 
 def projection_block(A_prev, filters, s=2):
     """
-    Build a projection block as described in Deep Residual Learning 
+    Build a projection block as described in Deep Residual Learning
     for Image Recognition (2015).
 
     Args:
