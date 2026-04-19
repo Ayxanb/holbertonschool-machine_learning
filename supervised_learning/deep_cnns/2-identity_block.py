8a81925ce5417cdfd3ffa4fb2aeb3c9779dfd3e9
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-04-19 18:58:24 +0000

    task

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -1,44 +1,82 @@
 #!/usr/bin/env python3
-"""
-Identity Block module for ResNet-50.
-"""
+
+'''
+This module contains `projection_block` function.
+'''
+
 from tensorflow import keras as K
 
 
-def identity_block(A_prev, filters):
+def projection_block(A_prev, filters, s=2):
     """
-    Build an identity block as described in Deep Residual Learning
+    Build a projection block as described in Deep Residual Learning 
     for Image Recognition (2015).
+
+    Args:
+        A_prev: tensor, output from the previous layer
+        filters: tuple or list containing f11, f3, f12
+            f11: number of filters in the first 1x1 convolution
+            f3: number of filters in the 3x3 convolution
+            f12: number of filters in the second 1x1 convolution and shortcut
+        s: stride of the first convolution in main and shortcut paths
+
+    Returns:
+        The activated output of the projection block
     """
     f11, f3, f12 = filters
-    init = K.initializers.HeNormal(seed=0)
+
+    # He Normal initializer with seed=0
+    initializer = K.initializers.HeNormal(seed=0)
+
+    # Save the input value to apply the shortcut convolution later
     shortcut = A_prev
 
-    # First component
+    # MAIN PATH
+    # First component of main path (1x1 convolution with stride=s)
     x = K.layers.Conv2D(
-        filters=f11, kernel_size=(1, 1), strides=(1, 1),
-        padding='valid', kernel_initializer=init
+        filters=f11,
+        kernel_size=(1, 1),
+        strides=(s, s),
+        padding='valid',
+        kernel_initializer=initializer
     )(A_prev)
     x = K.layers.BatchNormalization(axis=3)(x)
-    x = K.layers.ReLU()(x)
+    x = K.layers.Activation('relu')(x)
 
-    # Second component
+    # Second component of main path (3x3 convolution)
     x = K.layers.Conv2D(
-        filters=f3, kernel_size=(3, 3), strides=(1, 1),
-        padding='same', kernel_initializer=init
+        filters=f3,
+        kernel_size=(3, 3),
+        strides=(1, 1),
+        padding='same',
+        kernel_initializer=initializer
     )(x)
     x = K.layers.BatchNormalization(axis=3)(x)
-    x = K.layers.ReLU()(x)
+    x = K.layers.Activation('relu')(x)
 
-    # Third component
+    # Third component of main path (1x1 convolution)
     x = K.layers.Conv2D(
-        filters=f12, kernel_size=(1, 1), strides=(1, 1),
-        padding='valid', kernel_initializer=init
+        filters=f12,
+        kernel_size=(1, 1),
+        strides=(1, 1),
+        padding='valid',
+        kernel_initializer=initializer
     )(x)
     x = K.layers.BatchNormalization(axis=3)(x)
 
-    # Addition and final ReLU
+    # SHORTCUT PATH
+    # 1x1 convolution with stride=s to match dimensions of main path
+    shortcut = K.layers.Conv2D(
+        filters=f12,
+        kernel_size=(1, 1),
+        strides=(s, s),
+        padding='valid',
+        kernel_initializer=initializer
+    )(shortcut)
+    shortcut = keras.layers.BatchNormalization(axis=3)(shortcut)
+
+    # Final step: Add shortcut value to main path, THEN apply ReLU
     x = K.layers.Add()([x, shortcut])
-    x = K.layers.ReLU()(x)
-
+    x = K.layers.Activation('relu')(x)
+ 
     return x
