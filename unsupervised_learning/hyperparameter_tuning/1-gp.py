b9f3d7b6ed37579cf448db66b1ce495fb0f4034b
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-05-31 18:38:02 +0000

    task

---
+++ unsupervised_learning/hyperparameter_tuning/1-gp.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+This module provides the GaussianProcess class for performing
+1D Gaussian process regression, including prediction methods.
+"""
+import numpy as np
+
+
+class GaussianProcess:
+    """Represents a noiseless 1D Gaussian process."""
+
+    def __init__(self, X_init, Y_init, l=1, sigma_f=1):
+        """
+        Initializes the Gaussian Process.
+        """
+        self.X = X_init
+        self.Y = Y_init
+        self.l = l
+        self.sigma_f = sigma_f
+        self.K = self.kernel(self.X, self.X)
+
+    def kernel(self, X1, X2):
+        """
+        Calculates the covariance kernel matrix using the RBF kernel.
+        """
+        sqdist = (
+            np.sum(X1**2, 1).reshape(-1, 1)
+            + np.sum(X2**2, 1)
+            - 2 * np.dot(X1, X2.T)
+        )
+        return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
+
+    def predict(self, X_s):
+        """
+        Predicts the mean and standard deviation of points in a
+        Gaussian process.
+        """
+        K_s = self.kernel(self.X, X_s)
+        K_ss = self.kernel(X_s, X_s)
+        K_inv = np.linalg.inv(self.K)
+
+        # Mean: mu = K_s^T * K^-1 * Y
+        mu = K_s.T.dot(K_inv).dot(self.Y).flatten()
+
+        # Covariance: cov_s = K_ss - K_s^T * K^-1 * K_s
+        cov_s = K_ss - K_s.T.dot(K_inv).dot(K_s)
+
+        # Variance is the diagonal of the covariance matrix
+        sigma = np.diagonal(cov_s)
+
+        return mu, sigma
