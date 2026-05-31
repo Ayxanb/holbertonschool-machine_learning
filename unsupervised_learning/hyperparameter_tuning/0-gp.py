3c4e79db5ad3d5f45db719783f9ecd6c5e510096
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-05-31 18:37:29 +0000

    task

--- unsupervised_learning/hyperparameter_tuning/0-gp.py
+++ unsupervised_learning/hyperparameter_tuning/0-gp.py
@@ -25,11 +25,8 @@ class GaussianProcess:
         """
         # Calculate squared Euclidean distance:
         # dist = (x - y)^2 = x^2 + y^2 - 2xy
-        sqdist = (
-            np.sum(X1**2, 1).reshape(-1, 1)
-            +
-            np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
-        )
+        sqdist = np.sum(X1**2, 1).reshape(-1, 1) \
+            + np.sum(X2**2, 1) - 2 * np.dot(X1, X2.T)
 
         # RBF Kernel formula: K(x, y) = sigma_f^2 * exp(-0.5 * dist / l^2)
         return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
