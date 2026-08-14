712e9dd3a63fc9fcf3e658db5f074c7540893049
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-08-14 17:52:16 +0000

    task

---
+++ reinforcement_learning/q_learning/1-q_init.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def q_init(env):
+    """Initialize the Q-table with zeros for the given environment.
+
+    Args:
+        env: The Gymnasium environment instance.
+
+    Returns:
+        numpy.ndarray: A 2D array of zeros with shape
+            (number_of_states, number_of_actions).
+    """
+    num_states = env.observation_space.n
+    num_actions = env.action_space.n
+    return np.zeros((num_states, num_actions))
