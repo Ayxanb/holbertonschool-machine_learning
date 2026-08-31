9fdcaab28043faedf0557648f7b0a2c72c55803b
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-08-31 12:46:19 +0000

    task

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -1,49 +1,47 @@
 #!/usr/bin/env python3
-"""Module containing the Monte Carlo prediction algorithm."""
+"""Module that contains the Monte Carlo value estimation algorithm."""
+
 import numpy as np
 
 
-def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1,
-                gamma=0.99):
-    """Performs the Monte Carlo algorithm to update value estimates.
+def monte_carlo(
+    env, V, policy, episodes=5000, max_steps=100, alpha=0.1, gamma=0.99
+):
+    """Performs the Monte Carlo algorithm for value estimation.
 
     Args:
-        env: environment instance
-        V: numpy.ndarray of shape (s,) containing the value estimate
-        policy: function that takes in a state and returns next action
-        episodes: total number of episodes to train over
-        max_steps: maximum number of steps per episode
-        alpha: learning rate
-        gamma: discount rate
+        env: Environment instance.
+        V (numpy.ndarray): Array of shape (s,) containing value estimates.
+        policy (function): Function taking a state and returning action.
+        episodes (int): Total number of episodes to train over.
+        max_steps (int): Maximum number of steps per episode.
+        alpha (float): Learning rate.
+        gamma (float): Discount rate.
 
     Returns:
-        V: updated value estimate
+        numpy.ndarray: The updated value estimate V.
     """
     for _ in range(episodes):
         state = env.reset()
-        if isinstance(state, tuple) and len(state) == 2 and isinstance(
-                state[1], dict):
+        if isinstance(state, tuple):
             state = state[0]
 
         episode = []
         for _ in range(max_steps):
             action = policy(state)
             res = env.step(action)
-            next_state = res[0]
-            reward = res[1]
-            done = res[2]
-
+            next_state, reward, done = res[0], res[1], res[2]
             episode.append((state, reward))
             if done:
                 break
             state = next_state
 
         G = 0
-        for i in range(len(episode) - 1, -1, -1):
-            s, r = episode[i]
-            G = gamma * G + r
-            visited_states = [step[0] for step in episode[:i]]
-            if s not in visited_states:
-                V[s] = V[s] + alpha * (G - V[s])
+        states = [step[0] for step in episode]
+        for t in range(len(episode) - 1, -1, -1):
+            state, reward = episode[t]
+            G = gamma * G + reward
+            if state not in states[:t]:
+                V[state] = V[state] + alpha * (G - V[state])
 
     return V
