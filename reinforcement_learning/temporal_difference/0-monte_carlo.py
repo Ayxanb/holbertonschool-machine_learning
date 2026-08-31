3db7c8b7d0abe1bece54e586702fcafa78a2b603
Ayxanb <ayxanbeydiyev476@gmail.com>
2026-08-31 12:34:27 +0000

    task

--- reinforcement_learning/temporal_difference/0-monte_carlo.py
+++ reinforcement_learning/temporal_difference/0-monte_carlo.py
@@ -1,35 +1,41 @@
 #!/usr/bin/env python3
-"""Module containing the Monte Carlo prediction algorithm for RL."""
+"""Module containing the Monte Carlo prediction algorithm."""
 import numpy as np
 
 
 def monte_carlo(env, V, policy, episodes=5000, max_steps=100, alpha=0.1,
                 gamma=0.99):
-    """ Performs the Monte Carlo algorithm to update value estimates. """
+    """Performs the Monte Carlo algorithm to update value estimates.
+
+    Args:
+        env: environment instance
+        V: numpy.ndarray of shape (s,) containing the value estimate
+        policy: function that takes in a state and returns next action
+        episodes: total number of episodes to train over
+        max_steps: maximum number of steps per episode
+        alpha: learning rate
+        gamma: discount rate
+
+    Returns:
+        V: updated value estimate
+    """
     for _ in range(episodes):
         state = env.reset()
-        if isinstance(state, tuple):
-            state = state[0]
-
         episode = []
         for _ in range(max_steps):
             action = policy(state)
-            res = env.step(action)
-            next_state = res[0]
-            reward = res[1]
-            done = res[2]
-
-            episode.append((state, reward))
+            next_state, reward, done, _ = env.step(action)
+            episode.append([state, reward])
             if done:
                 break
             state = next_state
 
         G = 0
-        states = [step[0] for step in episode]
         for i in range(len(episode) - 1, -1, -1):
             s, r = episode[i]
             G = gamma * G + r
-            if s not in states[:i]:
+            visited_states = [step[0] for step in episode[:i]]
+            if s not in visited_states:
                 V[s] = V[s] + alpha * (G - V[s])
 
     return V
